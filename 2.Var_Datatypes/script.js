016a76f7e262000d8c38eab9d7d690eac73caa1d
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-22 16:03:57 +0530

    ...

---
+++ 2.Var_Datatypes/script.js
@@ -0,0 +1,45 @@
+//Q1. Declare the variable using let and log its value
+let aa = 12;
+console.log(aa);
+
+//Q2 Create a constant to store the value of PI and log it
+const pi  = Math.pi;
+console.log(pi);
+
+//Q3 Reassign a value to a variable declared with let and log the result
+let a = 13;
+a = 14;
+console.log(a);
+
+//Q4 Check the type of null and log it
+let b = null;
+console.log(typeof b);
+
+//Q5 Create a variable with number as a string and log its type
+let c = 12;
+console.log(typeof c);
+
+//Q6 Use typeof to check the type of boolean variable
+var d = true;
+console.log(typeof d);
+
+//Q7 Create three variable of types string , number , and boolean and their values
+let e = "hey"
+let f = 13
+let g = true;
+console.log(e,f,g);
+
+//Q8 Declare a variable without assigning a value and its type
+let ax;
+console.log(typeof ax);
+
+//Q9 Create a variable with undefined and log its type
+var az = undefined;
+console.log(az)
+
+//Typeof
+// 12 -> Number
+// himanshu -> String
+// true and false ->  Boolean
+// NaN  -> Number
+// null -> Object
